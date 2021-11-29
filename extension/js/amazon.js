fd76a7a07d49e77abd4dcf0f823682b14c56a54e
Dustin Shackley <dustinshackley@gmail.com>
2021-11-29 00:18:35 -0500

    Add cart, search, and product page price with tax functionality.

--- extension/js/amazon.js
+++ extension/js/amazon.js
@@ -5,21 +5,20 @@ function determinePage() {
     return 'cart';
   }
 
+  if (url.indexOf('/s?') !== -1) {
+    return 'search';
+  }
+
   return 'product';
 }
 
-function getPriceWithTax(page) {
-  if (page === 'product') {
-    const priceElem = document.querySelectorAll('.apexPriceToPay .a-offscreen')[0];
-    const price = priceElem.innerHTML;
-    const priceAsFloat = price.replace('$', '');
-    return priceAsFloat * 1.0675;
-  }
-
-  return null;
+function parsePriceElement(elem) {
+  const price = elem.innerHTML;
+  const priceAsFloat = price.replace('$', '');
+  return (priceAsFloat * 1.0675).toFixed(2);
 }
 
-function appendPriceToProductPage(priceWithTax) {
+function appendPriceToProductPage(elem, priceWithTax) {
   const tableRow = document.createElement('tr');
   const tableDataText = document.createElement('td');
   const tableDataPrice = document.createElement('td');
@@ -31,19 +30,79 @@ function appendPriceToProductPage(priceWithTax) {
   tableDataPrice.className = 'a-span12 a-color-price a-size-base';
 
   span.className = 'a-price a-text-price a-size-base';
-  span.innerText = `$${priceWithTax.toFixed(2)}`;
+  span.innerText = `$${priceWithTax}`;
 
   tableDataPrice.append(span);
 
   tableRow.append(tableDataText);
   tableRow.append(tableDataPrice);
 
-  document.querySelectorAll('.apexPriceToPay .a-offscreen')[0].closest('tbody').append(tableRow);
+  elem.closest('tbody').append(tableRow);
 }
 
-function addPriceToPage(page, priceWithTax) {
+function appendPriceToSearchPage(elem, priceWithTax) {
+  const div = document.createElement('div');
+  const span = document.createElement('span');
+
+  div.className = 'a-section a-spacing-none a-spacing-top-small';
+
+  span.className = 'a-price-fraction';
+  span.innerText = `With Tax: $${priceWithTax}`;
+
+  div.append(span);
+
+  const noTaxPriceContainer = elem.closest('.a-section .a-spacing-none .a-spacing-top-small');
+  noTaxPriceContainer.after(div);
+}
+
+function appendPriceToCartPage(elem, priceWithTax) {
+  const div = document.createElement('div');
+  const span = document.createElement('span');
+
+  div.className = 'a-section a-spacing-none a-spacing-top-small';
+
+  span.className = 'a-price-fraction';
+  span.innerText = `With Tax: $${priceWithTax}`;
+
+  div.append(span);
+
+  elem.parentNode.after(div);
+}
+
+function getPriceWithTax(page) {
   if (page === 'product') {
-    appendPriceToProductPage(priceWithTax);
+    const elem = document.querySelectorAll('.apexPriceToPay .a-offscreen')[0];
+    const price = parsePriceElement(elem);
+
+    if (price && price !== 'NaN') {
+      appendPriceToProductPage(elem, price);
+    }
+  }
+
+  if (page === 'search') {
+    const priceElems = document.querySelectorAll('.sg-col-inner .a-price .a-offscreen');
+    priceElems.forEach((d) => {
+      if (!d.parentNode.getAttribute('data-a-strike')) {
+        const price = parsePriceElement(d);
+
+        if (price && price !== 'NaN') {
+          appendPriceToSearchPage(d, price);
+        }
+      }
+    });
+  }
+
+  if (page === 'cart') {
+    const priceElems = document.querySelectorAll('[class*=sc-price]');
+    priceElems.forEach((d) => {
+      if (!d.parentNode.getAttribute('data-a-strike')) {
+        const price = parsePriceElement(d);
+
+        if (price && price !== 'NaN') {
+          appendPriceToCartPage(d, price);
+        }
+      }
+    });
   }
 }
 
@@ -51,9 +110,6 @@ function addPriceToPage(page, priceWithTax) {
   const page = determinePage();
 
   if (page) {
-    const priceWithTax = getPriceWithTax(page);
-    if (priceWithTax) {
-      addPriceToPage(page, priceWithTax);
-    }
+    getPriceWithTax(page);
   }
 }());
